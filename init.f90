e075886e64958c3ac40e38f0fb2ff8c032505a4c
wes <wes@fluidphysics.com>
2017-10-10 16:03:36 +0000

    change default values for selection_scheme and plot_allele_gens

--- init.f90
+++ init.f90
@@ -26,7 +26,7 @@ cyclic_bottlenecking = .false.
 ! Output version information.  RCS will automatically update
 ! the following $Id string on check-in
 
-write(6,*) 'VERSION >>> v2.7.1-63-g8aa4fc6-dirty <<< VERSION'
+write(6,*) 'VERSION >>> v2.7.2-1-gc0af1c8-dirty <<< VERSION'
 
 call date_and_time(VALUES=values)
 
